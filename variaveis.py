314d57c65e3eface6ed53c1a16788398e19ae4f5
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-03-27 18:11:49 -0300

    Feito criação do módulo variáveis

---
+++ variaveis.py
@@ -0,0 +1,22 @@
+# variaveis, como o proprio nome diz, sao locais (objetos) que estao sujeito à mudanças
+
+# variáveis podem ser criadas em diversos tipos:
+
+camelCase = "thisIsCamelCase"
+PascalCase = "PascalCase"
+snake_case = "snake_case (this is the most used in python)"
+
+
+print(camelCase)
+print(PascalCase)
+print(snake_case)
+
+
+num1 = 5
+num2 = 10
+soma = num1 + num2
+if soma == 13:
+    print("soma igual 13")
+
+else:
+    print(f"soma igual {soma}")
