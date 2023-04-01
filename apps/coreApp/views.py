4fe4a1d951d72ce68aaf9e7efa1619a23514c154
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2023-04-01 17:23:07 -0300

    adicionado Url

--- apps/coreApp/views.py
+++ apps/coreApp/views.py
@@ -1,9 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from apps.medicos.models import Medico
+from apps.pacientes.models import Paciente
+from apps.acompanhantes.models import Acompanhante
 
 # criar rota home
+
+
 @login_required(login_url="accounts/login")
 def home(request):
     return render(request, "index.html")
